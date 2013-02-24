92389ddc377701a2be709c484d63131a0928c3f0
Daniel <Daniel@Daniel-PC>
2013-02-24 00:12:02 -0300

    Atualização para uma aplicação de saudação

--- HelloWord/src/com/example/helloword/MainActivity.java
+++ HelloWord/src/com/example/helloword/MainActivity.java
@@ -2,21 +2,50 @@ package com.example.helloword;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+ 
+ public class MainActivity extends Activity {
 
-public class MainActivity extends Activity {
-
+	 private EditText nomeEditText;
+	 private TextView saudacaoText;
+	 private String saudacao;
+	 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        // setContentView(R.layout.activity_main);
        TextView view = new TextView(this);
-       view.setText("Hello Word");
-       setContentView(view);
-    
+       view.setText(R.id.teste);
+       setContentView(R.layout.activity_main);
+       
+       this.nomeEditText = (EditText)findViewById(R.id.nomeInformado);
+       
+       this.saudacaoText = (TextView)findViewById(R.id.saudacao);
+       
+       this.saudacao = getResources().getString(R.string.saudacao);
+       
+       
+       
     }
 
+    
+    public void informaNome(View v){
+    	
+    	String nome = this.nomeEditText.getText().toString();
+    	
+    	String texto = this.saudacao+" "+nome;
+    	
+    	this.saudacaoText.setText(texto);
+    	
+    	
+    	
+    	
+    	
+    }
 
  /*   @Override
     public boolean onCreateOptionsMenu(Menu menu) {
